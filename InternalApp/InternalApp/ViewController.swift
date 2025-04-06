1d0034e12b63807ecac7a581be95e3f31b643e4e
Eslam Nasser <eslamoz.nasser@gmail.com>
2025-04-06 14:17:28 +0200

    create example package

---
+++ InternalApp/InternalApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  InternalApp
+//
+//  Created by Eslam Nasser on 2025-04-06.
+//
+
+import UIKit
+import InternalAppPackage
+
+class ViewController: UIViewController {
+
+    let pewPew = PewPew()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+        pewPew.pew() // should print pew
+    }
+}
+
---
+++ InternalAppPackage/Sources/InternalAppPackage/PewPew.swift
@@ -0,0 +1,17 @@
+//
+//  PewPew.swift
+//  InternalAppPackage
+//
+//  Created by Eslam Nasser on 2025-04-06.
+//
+
+import Foundation
+
+public class PewPew {
+
+    public init() {}
+
+    public func pew() {
+        debugPrint("pew pew")
+    }
+}
